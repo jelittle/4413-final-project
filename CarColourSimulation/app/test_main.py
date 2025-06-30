d3839e9b118c5d56f816dcf11db78a94e597cbdc
Joshua little <joshlittle077@gmail.com>
2025-06-30 01:29:31 -0400

    first real test

--- CarColourSimulation/app/test_main.py
+++ CarColourSimulation/app/test_main.py
@@ -2,24 +2,29 @@ from fastapi.testclient import TestClient
 
 from fastapi.testclient import TestClient
 from .app import app
-import pytest
+
 import time
 
 client = TestClient(app)
 #add tests: for grpc requirement
 def test_returns_image():
     """Test that the endpoint returns an image"""
-    response = client.get("/generate-car")
+    test_image_path = "CarColourSimulation/app/test_data/car2_1.jpg"
+    
+    with open(test_image_path, "rb") as f:
+        response = client.post(
+            "/simulate_colour",
+            files={"file": ("car2_1.jpg", f, "image/jpeg")},
+            data={"colour": "[108, 148, 252]"}  # Red color as JSON string
+        )
+    
     assert response.status_code == 200
-    assert response.headers["content-type"].startswith("image/")
+    assert "image" in response.json()
+
 
 def test_image_is_different():
     """Test that multiple calls return different images"""
-    response1 = client.get("/generate-car")
-    response2 = client.get("/generate-car")
-    assert response1.status_code == 200
-    assert response2.status_code == 200
-    assert response1.content != response2.content
+    pass
 
 
 
